8a8a15590d509bcac1d85ce4ef048176dea91e91
Aaron Klinker <aaronklinker1@gmail.com>
2019-12-29 16:53:22 -0600

    Removed redundant ctx reset

--- internal/handlers/graphql.go
+++ internal/handlers/graphql.go
@@ -1,14 +1,11 @@
 package handlers
 
 import (
-	"context"
-
 	"github.com/99designs/gqlgen/handler"
 	"github.com/aklinker1/anime-skip-backend/internal/database"
 	gql "github.com/aklinker1/anime-skip-backend/internal/graphql"
 	"github.com/aklinker1/anime-skip-backend/internal/graphql/directives"
 	"github.com/aklinker1/anime-skip-backend/internal/graphql/resolvers"
-	"github.com/aklinker1/anime-skip-backend/internal/utils/constants"
 	"github.com/gin-gonic/gin"
 )
 
@@ -26,8 +23,7 @@ func GraphQLHandler(orm *database.ORM) gin.HandlerFunc {
 	// Apply and serve the schema
 	handler := handler.GraphQL(gql.NewExecutableSchema(config))
 	return func(c *gin.Context) {
-		ctx := context.WithValue(c.Request.Context(), constants.CTX_GIN_CONTEXT, c)
-		handler.ServeHTTP(c.Writer, c.Request.WithContext(ctx))
+		handler.ServeHTTP(c.Writer, c.Request)
 	}
 }
 
