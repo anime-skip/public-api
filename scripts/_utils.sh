30b3307a9f431c87a527cc755990eb6cfc1c7389
Aaron Klinker <aaronklinker1@gmail.com>
2020-01-04 10:49:23 -0600

    Added README.md, added init and help make rules

--- scripts/_utils.sh
+++ scripts/_utils.sh
@@ -6,3 +6,24 @@ function loadENV {
 function unloadENV {
     unset $(grep -v '^#' .env | sed -E 's/(.*)=.*/\1/' | xargs)
 }
+
+function header {
+    echo ""
+    echo -e "\x1b[1m\x1b[94m$1\x1b[0m"
+}
+
+function warning {
+    echo -e " \x1b[93m▶ $1\x1b[0m"
+}
+
+function bullet {
+    echo -e " • $1"
+}
+
+function underline {
+    echo -en "\x1b[4m$1\x1b[0m"
+}
+
+function codeBlock {
+    echo -en "\x1b[1m\x1b[2m\x1b[3m$1\x1b[0m"
+}
