68729f105081c73f8485566235cfe63f0351ac1f
Aaron Klinker <aaronklinker1@gmail.com>
2020-01-04 10:52:18 -0600

    Added missing help and init scripts

---
+++ scripts/help.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+source scripts/_utils.sh
+
+header "Available \x1b[3mmake\x1b[0m\x1b[1m\x1b[94m rules"
+
+function makeRule {
+  bullet "$(codeBlock "$1") - $2"
+}
+makeRule "make build" "Build the source code to $(underline build/anime-skip-backend) (default, can be ran with just $(codeBlock make))"
+makeRule "make run  " "Run the server"
+makeRule "make watch" "Run the server and restart it when a file changes"
+makeRule "make gen  " "Run gqlgen to generate GraphQL models and resolvers"
+makeRule "make init " "Initialize a $(underline .env) file and show $(codeBlock make) commands"
+makeRule "make help " "show $(codeBlock make) commands"
+
+echo ""
---
+++ scripts/init.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+source scripts/_utils.sh
+
+header "Creating .env"
+if [ -f ".env" ]; then
+    warning ".env already exists, skipping"
+else
+  echo "# General
+IS_DEV=true
+ENABLE_PLAYGROUND=true
+ENABLE_INTROSPECTION=true
+LOG_LEVEL=0
+LOG_SQL=false
+
+# Web Server
+HOST=localhost
+PORT=8000
+GIN_MODE=release
+
+# Postgres
+POSTGRES_HOST=localhost
+POSTGRES_PORT=5432
+POSTGRES_USER=<username>
+POSTGRES_PASSWORD=<password>
+POSTGRES_DBNAME=anime_skip
+POSTGRES_DISABLE_SSL=true
+POSTGRES_ENABLE_SEEDING=true
+
+# Secrets
+JWT_SECRET=<get_from_aaron>" > .env
+fi
+
+./scripts/help.sh
