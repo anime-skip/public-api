7825161c5e343aed0aca164c6adc64060f4643c6
Aaron Klinker <aaronklinker1@gmail.com>
2020-01-04 18:07:53 -0600

    Welcome email helper

---
+++ internal/server/email/account_emails.go
@@ -0,0 +1,28 @@
+package email
+
+import "github.com/aklinker1/anime-skip-backend/internal/database/entities"
+
+func SendWelcome(account *entities.User) error {
+	email := Email{
+		To:       []string{account.Email},
+		Subject:  "Welcome to Anime Skip",
+		Template: "email_welcome.html",
+		TemplateData: map[string]string{
+			"username": account.Username,
+		},
+	}
+	return email.Send()
+}
+
+func SendEmailAddressValidation(account *entities.User, token string) error {
+	email := Email{
+		To:       []string{account.Email},
+		Subject:  "Validate Email Address",
+		Template: "email_validate.hmtl",
+		TemplateData: map[string]string{
+			"username": account.Username,
+			"token":    token,
+		},
+	}
+	return email.Send()
+}
