58b8c331b00a40f9149b6e26c9a8e2d70239be07
Aaron Klinker <aaronklinker1@gmail.com>
2020-01-05 11:06:14 -0600

    Switch build output from build/ to bin/

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 INPUT="cmd/anime-skip-backend/main.go"
-OUTPUT="build/anime-skip-backend"
+OUTPUT="bin/anime-skip-backend"
 
 go build -o $OUTPUT $INPUT
 SIZE="$(ls -lah $OUTPUT | awk '{print $5}')"
