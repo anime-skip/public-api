da4198f7e15db35dd54688b7a6de51585f7ebf71
Aaron Klinker <aaronklinker1@gmail.com>
2020-08-31 23:59:10 -0500

    Migration to lowercase all the existing emails

---
+++ internal/database/migrations/tables/09_lowercase_all_emails.go
@@ -0,0 +1,13 @@
+package tables
+
+var LowercaseAllEmails = migrateTableChange(
+	"MODIFY_USERS_TABLE__lowercase_emails",
+	[]string{
+		"UPDATE public.users",
+		"SET email=lower(email);",
+	},
+	[]string{
+		// Cannot undo this migration
+		"SELECT count(*) FROM USERS",
+	},
+)
