7b5bf9b23e8f222e78ac59d78a3dd6100fb062c7
Aaron Klinker <aaronklinker1@gmail.com>
2020-09-01 00:11:58 -0500

    Updated deploy remote

--- .github/deploy.sh
+++ .github/deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-git push https://heroku:$HEROKU_API_KEY@git.heroku.com/anime-skip-backend.git origin/master:master
+git push https://heroku:$HEROKU_API_KEY@git.heroku.com/anime-skip-api.git origin/master:master
