a61a71a3fafcd77fea04bcaf6373b631ece7760a
Aaron Klinker <aaronklinker1@gmail.com>
2020-11-14 12:16:41 -0600

    fix: Fix old migartions to have a rollback strategy

--- internal/database/migrations/tables/11_timestamp_source.go
+++ internal/database/migrations/tables/11_timestamp_source.go
@@ -1,13 +1,16 @@
 package tables
 
 // AddTimestampSource inserts new source column with a default value
-var AddTimestampSource = migrateTable(
+var AddTimestampSource = migrateTableChange(
 	"MODIFY_TIMESTAMPS_TABLE__add_source",
-	"timestamps",
 	[]string{
 		"ALTER TABLE public.timestamps",
 		"ADD source integer NOT NULL",
 		"CONSTRAINT source_default_value",
 		"DEFAULT 0;",
+	},
+	[]string{
+		"ALTER TABLE public.timestamps",
+		"DROP COLUMN source;",
 	},
 )
--- internal/database/migrations/tables/12_episode_durations.go
+++ internal/database/migrations/tables/12_episode_durations.go
@@ -1,11 +1,14 @@
 package tables
 
 // AddDurationToEpisodes inserts new base_duration column with a default value
-var AddBaseDurationToEpisodes = migrateTable(
+var AddBaseDurationToEpisodes = migrateTableChange(
 	"MODIFY_EPISODES_TABLE__add_base_duration",
-	"episodes",
 	[]string{
 		"ALTER TABLE public.episodes",
 		"ADD base_duration decimal;",
+	},
+	[]string{
+		"ALTER TABLE public.episodes",
+		"DROP COLUMN base_duration;",
 	},
 )
