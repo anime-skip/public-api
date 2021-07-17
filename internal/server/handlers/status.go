cb870be173bc3ed5044987a89af7eb915dcc8be6
Aaron Klinker <aaronklinker1@gmail.com>
2021-07-17 10:23:56 -0500

    fix: include version date and hash in production
    Also fix the displayed value, add a `-` between the version and date

--- internal/server/handlers/status.go
+++ internal/server/handlers/status.go
@@ -11,10 +11,7 @@ import (
 // Status is the handler that can be easily reached to tell if the application is running
 func Status() gin.HandlerFunc {
 	isPlaygroundEnabled := env.ENABLE_PLAYGROUND
-	version := constants.VERSION
-	if env.IS_DEV || env.IS_STAGED {
-		version += constants.VERSION_SUFFIX
-	}
+	version := constants.VERSION + "-" + constants.VERSION_SUFFIX
 	return func(c *gin.Context) {
 		statusData := map[string]interface{}{
 			"status":        "RUNNING",
