699f769d6023953ef91e90a06f76383c648a4678
Aaron Klinker <aaronklinker1@gmail.com>
2021-10-11 11:56:00 -0500

    chore: update client generation script to include rate limitting

--- cmd/generate-api-client/main.go
+++ cmd/generate-api-client/main.go
@@ -33,14 +33,24 @@ func main() {
 	userId, _ := reader.ReadString('\n')
 	userId = strings.TrimSpace(userId)
 
+	fmt.Print("\nRate limit? (Y/n) ")
+	isRateLimit, _ := reader.ReadString('\n')
+	isRateLimit = strings.TrimSpace(isRateLimit)
+	rateLimit := "NULL"
+	if strings.ToLower(isRateLimit) == "y" {
+		fmt.Print("\nEnter a requests per minute: (60) ")
+		rateLimit, _ = reader.ReadString('\n')
+		rateLimit = strings.TrimSpace(rateLimit)
+	}
+
 	fmt.Printf(`
 Run and COMMIT; the following query:
 
   BEGIN TRANSACTION; INSERT INTO api_clients (
-    id, created_at, created_by_user_id, updated_at, updated_by_user_id, user_id, app_name, description
+    id, created_at, created_by_user_id, updated_at, updated_by_user_id, user_id, app_name, description, rate_limit_rpm
   ) VALUES (
-    '%s' NOW, '389a3749-c8f4-4e39-bf5f-96c1c2452074', NOW, '389a3749-c8f4-4e39-bf5f-96c1c2452074', '%s', '%s', '%s'
+    '%s', now(), '389a3749-c8f4-4e39-bf5f-96c1c2452074', now(), '389a3749-c8f4-4e39-bf5f-96c1c2452074', '%s', '%s', '%s', %s
   );
-`, id, userId, appName, description)
+`, id, userId, appName, description, rateLimit)
 	fmt.Println()
 }
