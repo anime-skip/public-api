b4e22d16fc83a0a8f3a385bc681e38fc2967b111
Aaron Klinker <aaronklinker1@gmail.com>
2022-02-26 15:48:02 -0600

    improve recently added episodes query

--- internal/postgres/episode_repo.go
+++ internal/postgres/episode_repo.go
@@ -24,5 +24,42 @@ func getEpisodeSeasonCountByShowID(ctx context.Context, db internal.Database, id
 }
 
 func getRecentlyAddedEpisodes(ctx context.Context, db internal.Database, params internal.GetRecentlyAddedParams) ([]internal.Episode, error) {
-	panic("not implemented")
+	// What is this query?
+	// 1. Grab timestamps with distinct episode ids
+	// 2. Grab just the episode_id the timestamp belongs to and sort them by newest first (this is
+	//    where we apply pagination)
+	// 3. Select all the episodes with those ids, making sure to sort them again since the
+	//    timestamps' created_at can be in a different order than the episodes' created_at
+	//
+	// This isn't perfect (episode could be missing or slightly out of order), but it beats what the
+	// the old query - 13ms vs 10s
+	// https://github.com/anime-skip/backend/blob/33fd2b842bc847bed67c9b1f9283e78b710cd1f5/internal/database/repos/episodes.go#L137-L153
+	query := `
+		SELECT * FROM episodes
+		WHERE id IN (
+			SELECT episode_id
+			FROM (SELECT DISTINCT ON (episode_id) * FROM timestamps) as episode_ids
+			ORDER BY created_at DESC NULLS LAST
+			LIMIT $1
+			OFFSET $2
+		)
+		ORDER BY created_at DESC NULLS LAST;
+	`
+	rows, err := db.QueryxContext(ctx, query, params.Limit, params.Offset)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	episodes := []internal.Episode{}
+	for rows.Next() {
+		var episode internal.Episode
+		err = rows.StructScan(&episode)
+		if err != nil {
+			return nil, err
+		}
+		episodes = append(episodes, episode)
+	}
+
+	return episodes, nil
 }
