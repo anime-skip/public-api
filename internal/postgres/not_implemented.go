b3a1f4ff87161c7926c4ebbcd6dd724e57267d90
Aaron Klinker <aaronklinker1@gmail.com>
2022-05-21 16:12:42 -0500

    templates implemented

--- internal/postgres/not_implemented.go
+++ internal/postgres/not_implemented.go
@@ -7,62 +7,6 @@ import (
 	uuid "github.com/gofrs/uuid"
 )
 
-// EpisodeURL
-
-func findEpisodeURLs(ctx context.Context, tx internal.Tx, filter internal.EpisodeURLsFilter) ([]internal.EpisodeURL, error) {
-	return []internal.EpisodeURL{}, internal.NewNotImplemented("findEpisodeURLs")
-}
-
-func findEpisodeURL(ctx context.Context, tx internal.Tx, filter internal.EpisodeURLsFilter) (internal.EpisodeURL, error) {
-	all, err := findEpisodeURLs(ctx, tx, filter)
-	if err != nil {
-		return internal.ZeroEpisodeURL, err
-	} else if len(all) == 0 {
-		return internal.ZeroEpisodeURL, internal.NewNotFound("Episode URL", "findEpisodeURL")
-	}
-	return all[0], nil
-}
-
-func createEpisodeURL(ctx context.Context, tx internal.Tx, episodeURL internal.EpisodeURL, createdBy uuid.UUID) (internal.EpisodeURL, error) {
-	return episodeURL, internal.NewNotImplemented("createEpisodeURL")
-}
-
-func updateEpisodeURL(ctx context.Context, tx internal.Tx, episodeURL internal.EpisodeURL, updatedBy uuid.UUID) (internal.EpisodeURL, error) {
-	return episodeURL, internal.NewNotImplemented("updateEpisodeURL")
-}
-
-func deleteEpisodeURL(ctx context.Context, tx internal.Tx, episodeURL internal.EpisodeURL) (internal.EpisodeURL, error) {
-	return episodeURL, internal.NewNotImplemented("deleteEpisodeURL")
-}
-
-// Template
-
-func findTemplates(ctx context.Context, tx internal.Tx, filter internal.TemplatesFilter) ([]internal.Template, error) {
-	return []internal.Template{}, internal.NewNotImplemented("findTemplates")
-}
-
-func findTemplate(ctx context.Context, tx internal.Tx, filter internal.TemplatesFilter) (internal.Template, error) {
-	all, err := findTemplates(ctx, tx, filter)
-	if err != nil {
-		return internal.ZeroTemplate, err
-	} else if len(all) == 0 {
-		return internal.ZeroTemplate, internal.NewNotFound("Template", "findTemplate")
-	}
-	return all[0], nil
-}
-
-func createTemplate(ctx context.Context, tx internal.Tx, template internal.Template, createdBy uuid.UUID) (internal.Template, error) {
-	return template, internal.NewNotImplemented("createTemplate")
-}
-
-func updateTemplate(ctx context.Context, tx internal.Tx, template internal.Template, updatedBy uuid.UUID) (internal.Template, error) {
-	return template, internal.NewNotImplemented("updateTemplate")
-}
-
-func deleteTemplate(ctx context.Context, tx internal.Tx, template internal.Template, deletedBy uuid.UUID) (internal.Template, error) {
-	return template, internal.NewNotImplemented("deleteTemplate")
-}
-
 // TemplateTimestamp
 
 func findTemplateTimestamps(ctx context.Context, tx internal.Tx, filter internal.TemplateTimestampsFilter) ([]internal.TemplateTimestamp, error) {
@@ -117,32 +61,4 @@ func updateTimestamp(ctx context.Context, tx internal.Tx, timestamp internal.Tim
 
 func deleteTimestamp(ctx context.Context, tx internal.Tx, timestamp internal.Timestamp, deletedBy uuid.UUID) (internal.Timestamp, error) {
 	return timestamp, internal.NewNotImplemented("deleteTimestamp")
-}
-
-// TimestampType
-
-func findTimestampTypes(ctx context.Context, tx internal.Tx, filter internal.TimestampTypesFilter) ([]internal.TimestampType, error) {
-	return []internal.TimestampType{}, internal.NewNotImplemented("findTimestampTypes")
-}
-
-func findTimestampType(ctx context.Context, tx internal.Tx, filter internal.TimestampTypesFilter) (internal.TimestampType, error) {
-	all, err := findTimestampTypes(ctx, tx, filter)
-	if err != nil {
-		return internal.ZeroTimestampType, err
-	} else if len(all) == 0 {
-		return internal.ZeroTimestampType, internal.NewNotFound("Timestamp type", "findTimestampType")
-	}
-	return all[0], nil
-}
-
-func createTimestampType(ctx context.Context, tx internal.Tx, timestampType internal.TimestampType, createdBy uuid.UUID) (internal.TimestampType, error) {
-	return timestampType, internal.NewNotImplemented("createTimestampType")
-}
-
-func updateTimestampType(ctx context.Context, tx internal.Tx, timestampType internal.TimestampType, updatedBy uuid.UUID) (internal.TimestampType, error) {
-	return timestampType, internal.NewNotImplemented("updateTimestampType")
-}
-
-func deleteTimestampType(ctx context.Context, tx internal.Tx, timestampType internal.TimestampType, deletedBy uuid.UUID) (internal.TimestampType, error) {
-	return timestampType, internal.NewNotImplemented("deleteTimestampType")
 }
--- internal/postgres/template_repo.go
+++ internal/postgres/template_repo.go
@@ -5,8 +5,136 @@ import (
 
 	"anime-skip.com/public-api/internal"
 	"anime-skip.com/public-api/internal/log"
+	"anime-skip.com/public-api/internal/postgres/sqlbuilder"
+	"anime-skip.com/public-api/internal/utils"
 	uuid "github.com/gofrs/uuid"
 )
+
+func findTemplates(ctx context.Context, tx internal.Tx, filter internal.TemplatesFilter) ([]internal.Template, error) {
+	var scanned internal.Template
+	query := sqlbuilder.Select("templates", map[string]any{
+		"id":                 &scanned.ID,
+		"created_at":         &scanned.CreatedAt,
+		"created_by_user_id": &scanned.CreatedByUserID,
+		"updated_at":         &scanned.UpdatedAt,
+		"updated_by_user_id": &scanned.UpdatedByUserID,
+		"deleted_at":         &scanned.DeletedAt,
+		"deleted_by_user_id": &scanned.DeletedByUserID,
+		"show_id":            &scanned.ShowID,
+		"source_episode_id":  &scanned.SourceEpisodeID,
+		"type":               &scanned.Type,
+	})
+	if filter.IncludeDeleted {
+		query.IncludeSoftDeleted()
+	}
+	if filter.ID != nil {
+		query.Where("id = ?", *filter.ID)
+	}
+	if filter.Pagination != nil {
+		query.Paginate(*filter.Pagination)
+	}
+	query.OrderBy("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX", "ASC")
+
+	sql, args := query.ToSQL()
+	rows, err := tx.QueryContext(ctx, sql, args...)
+	if err != nil {
+		return nil, internal.SQLFailure("findTemplates", err)
+	}
+	dest := query.ScanDest()
+	result := make([]internal.Template, 0)
+	for rows.Next() {
+		err = rows.Scan(dest...)
+		if err != nil {
+			return nil, internal.SQLFailure("findTemplates", err)
+		}
+		result = append(result, scanned)
+	}
+	return result, rows.Err()
+}
+
+func findTemplate(ctx context.Context, tx internal.Tx, filter internal.TemplatesFilter) (internal.Template, error) {
+	all, err := findTemplates(ctx, tx, filter)
+	if err != nil {
+		return internal.ZeroTemplate, err
+	} else if len(all) == 0 {
+		return internal.ZeroTemplate, internal.NewNotFound("Template", "findTemplate")
+	}
+	return all[0], nil
+}
+
+func createTemplate(ctx context.Context, tx internal.Tx, template internal.Template, createdBy uuid.UUID) (internal.Template, error) {
+	id, err := utils.RandomID()
+	if err != nil {
+		return template, err
+	}
+	template.ID = id
+	template.CreatedAt = *now()
+	template.CreatedByUserID = &createdBy
+	template.UpdatedAt = *now()
+	template.UpdatedByUserID = &createdBy
+
+	sql, args := sqlbuilder.Insert("templates", map[string]any{
+		"id":                 template.ID,
+		"created_at":         template.CreatedAt,
+		"created_by_user_id": template.CreatedByUserID,
+		"updated_at":         template.UpdatedAt,
+		"updated_by_user_id": template.UpdatedByUserID,
+		"show_id":            template.ShowID,
+		"source_episode_id":  template.SourceEpisodeID,
+		"type":               template.Type,
+	}).ToSQL()
+
+	_, err = tx.ExecContext(ctx, sql, args...)
+	if isConflict(err) {
+		return template, &internal.Error{
+			Code:    internal.ECONFLICT,
+			Message: "Template with the generated UUID already exists, try again",
+			Op:      "createTemplate",
+			Err:     err,
+		}
+	} else if err != nil {
+		return template, &internal.Error{
+			Code:    internal.EINTERNAL,
+			Message: "Failed to create Template",
+			Op:      "createTemplate",
+			Err:     err,
+		}
+	}
+	return template, nil
+}
+
+func updateTemplate(ctx context.Context, tx internal.Tx, template internal.Template, updatedBy uuid.UUID) (internal.Template, error) {
+	template.UpdatedAt = *now()
+	template.UpdatedByUserID = &updatedBy
+
+	sql, args := sqlbuilder.Update("templates", template.ID, map[string]any{
+		"updated_at":         template.UpdatedAt,
+		"updated_by_user_id": template.UpdatedByUserID,
+		"deleted_at":         template.DeletedAt,
+		"deleted_by_user_id": template.DeletedByUserID,
+		"show_id":            template.ShowID,
+		"source_episode_id":  template.SourceEpisodeID,
+		"type":               template.Type,
+	}).ToSQL()
+
+	_, err := tx.ExecContext(ctx, sql, args...)
+	if err != nil {
+		return template, &internal.Error{
+			Code:    internal.EINTERNAL,
+			Message: "Failed to update Template",
+			Op:      "updateTemplate",
+			Err:     err,
+		}
+	}
+
+	return template, nil
+}
+
+func deleteTemplate(ctx context.Context, tx internal.Tx, template internal.Template, deletedBy uuid.UUID) (internal.Template, error) {
+	template.DeletedByUserID = &deletedBy
+	template.DeletedAt = now()
+	return updateTemplate(ctx, tx, template, deletedBy)
+}
 
 func deleteCascadeTemplate(ctx context.Context, tx internal.Tx, template internal.Template, deletedBy uuid.UUID) (internal.Template, error) {
 	log.V("Deleting template: %v", template.ID)
